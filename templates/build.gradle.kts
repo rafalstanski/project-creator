b7bc33e16ab51beda326cdeaa576c12a49993f13
rafalstanski <nominis@o2.pl>
2026-09-14 20:43:23 +0200

    #36: adding uber-jar to gradle template file

--- templates/build.gradle.kts
+++ templates/build.gradle.kts
@@ -19,6 +19,17 @@ application {
 }
 
 kotlin {
-    jvmToolchain({{JAVA_VERSION}})
     // Supported JVM versions for Kotlin {{KOTLIN_VERSION}}: {{JAVA_VERSIONS}}
+    jvmToolchain({{JAVA_VERSION}})
+}
+
+// Creates uber-jar. No external libs. Alternative: use `shadow` plugin (https://gradleup.com/shadow/)
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = application.mainClass.get()
+    }
+    // Fixes duplicates if multiple dependencies have the same file (like module-info.class)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    // Collects all dependencies from the runtime classpath and zips them into the Jar
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
