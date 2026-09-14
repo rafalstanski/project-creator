6749b0c52564ea0c5cf90f899a86cd6fb5da413c
rafalstanski <nominis@o2.pl>
2026-09-14 18:06:33 +0200

    #34: adding support for tool and dev mode

--- project_creator/java_versions.py
+++ project_creator/java_versions.py
@@ -14,9 +14,9 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import cast
 
-JAVA_VERSIONS_FILE = (
-    Path(__file__).resolve().parents[1] / ".cache" / "java_versions.json"
-)
+from project_creator.paths import cache_dir
+
+JAVA_VERSIONS_FILE = cache_dir() / "java_versions.json"
 KOTLIN_COMPILER_RELEASES_URL = (
     "https://github.com/JetBrains/kotlin/releases/download/"
     "v{kotlin_version}/kotlin-compiler-{kotlin_version}.zip"
